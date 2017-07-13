7ab3f447f0f202842f7662eb136bd72c738a5f30
Jacob Burenstam <burenstam@gmail.com>
2017-07-13 19:25:13 +0200

    Add rack-cors to control CORS headers

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,12 @@ module LunchAndLearn
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i(get post options)
+      end
+    end
   end
 end
